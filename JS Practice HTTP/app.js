a2034adaf9f77a0f4454fa36af5a314ffae8eb6f
apollinariiy <95570468+apollinariiy@users.noreply.github.com>
2024-07-02 14:01:48 +0300

    task for HTTP module

---
+++ JS Practice HTTP/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const service = require('./service');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+
+const city = 'London';
+
+app.get('/200', async (req, res) => {
+    try {
+        const data = await service.getWeather({ q: city, appid: process.env.API_KEY });
+        res.json(data);
+    } catch (error) {
+        res.status(error.status).send(error.data);
+    }
+});
+
+app.get('/400', async (req, res) => {
+    try {
+        const data = await service.getWeather({ appid: process.env.API_KEY });
+        res.json(data);
+    } catch (error) {
+        res.status(error.status).send(error.data);
+    }
+});
+
+app.get('/404', async (req, res) => {
+    try {
+        const data = await service.getWeather({ q: city + 'n', appid: process.env.API_KEY });
+        res.json(data);
+    } catch (error) {
+        res.status(error.status).send(error.data);
+    }
+});
+
+app.get('/401', async (req, res) => {
+    try {
+        const data = await service.getWeather({ q: city });
+        res.json(data);
+    } catch (error) {
+        res.status(error.status).send(error.data);
+    }
+});
+
+app.put('/405', async (req, res) => {
+    const { data } = req.body;
+    try {
+        const result = await service.updateWeather(data, { q: city, appid: process.env.API_KEY });
+        res.json(result);
+    } catch (error) {
+        res.status(error.status).send(error.data);
+    }
+});
+
+app.listen(3000);
